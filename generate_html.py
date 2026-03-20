38e10c3c0c6fd345bc0b039c971ee97f5308b163
Javier Palacios <javijpa@hotmail.com>
2026-03-20 17:59:17 +0100

    .

--- generate_html.py
+++ generate_html.py
@@ -15,13 +15,14 @@ with open("index.html", "w", encoding="utf8") as out_file:
 	url_text = ""
 	count = 0
 	for i, f in enumerate(files):
+		f = f.replace("\\", "/")
 		match = re.search(r"\[([\d\w_]+)\]\.mp3", f)
 		if match and match.re.groups == 1:
 			url = match[1]
 			index = f.find(match[0])
 			name = f[0:f.find(match[0])]
 			text += f"""
-			<button onclick="toggle_video('{count}')">{os.path.basename(name)}</button>
+			<button onclick="toggle_video(this, '{count}')">{os.path.basename(name)}</button>
 			<br>
 			"""
 			if count > 0:
@@ -29,7 +30,6 @@ with open("index.html", "w", encoding="utf8") as out_file:
 			url_text += f"'{url}'"
 
 			count += 1
-			if count > 5: break
 
 	final_text = template.replace("{1}", text)
 	final_text = final_text.replace("{2}", f"const url_arr = [{url_text}]")
